d821cc4fd1df1c2dda1c9540bd4807eacf6fbc9f
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-09-15 19:36:13 +0300

    refactor

--- src/test/java/org/example/SqlHelperTest.java
+++ src/test/java/org/example/SqlHelperTest.java
@@ -3,7 +3,10 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -32,23 +35,39 @@ class SqlHelperTest {
         assertTrue(timestamps.size() > 0);
     }
 
+    private static final ZoneId UTC = ZoneId.of("UTC");
+    private static final ZoneId EU_STOCKHOLM = ZoneId.of("Europe/Stockholm");
+    private static final ZoneId EU_HELSINKI = ZoneId.of("Europe/Helsinki");
+
     @Test
     void shouldInsertARow() {
+        final LocalDate eventDate = LocalDate.of(2018, 8, 9);
+        final LocalTime eventTimeInStockholm = LocalTime.of(14, 22);
         final ZonedDateTime zonedDateTime = LocalDateTime
-                .of(2018, 8, 9, 14, 22)
-                .atZone(ZoneId.of("Europe/Stockholm"));
-        final Timestamp tsInStockholmTimeZone = Timestamp.from(zonedDateTime.toInstant());
+                .of(eventDate, eventTimeInStockholm)
+                .atZone(EU_STOCKHOLM);
+
+        final Timestamp eventTimestamp = Timestamp.from(zonedDateTime.toInstant());
 
         int count = updateOrInsert("insert into event values(?,?)",
                 stringParam(1, "Conference"),
-                timestampParam(2, tsInStockholmTimeZone)
+                timestampParam(2, eventTimestamp)
         );
         assertEquals(1, count);
 
-        Optional<Timestamp> maybeTs = selectOne("select timestamp from event where description = 'Conference'", getTimestamp("timestamp"));
-        assertTrue(maybeTs.isPresent());
-        Timestamp tsFromDb = maybeTs.get();
+        Optional<Timestamp> maybeEventTimestamp = selectOne(
+                "select timestamp from event where description = 'Conference'",
+                getTimestamp("timestamp")
+        );
+        assertTrue(maybeEventTimestamp.isPresent());
+        final Instant persistedEventInstant = maybeEventTimestamp.get().toInstant();
 
-        assertEquals(tsInStockholmTimeZone, tsFromDb);
+        ZonedDateTime eventDateTimeUTC = ZonedDateTime.ofInstant(persistedEventInstant, UTC);
+        ZonedDateTime eventDateTimeStockholm = ZonedDateTime.ofInstant(persistedEventInstant, EU_STOCKHOLM);
+        ZonedDateTime eventDateTimeHelsinki = ZonedDateTime.ofInstant(persistedEventInstant, EU_HELSINKI);
+
+        assertEquals(eventTimeInStockholm, eventDateTimeStockholm.toLocalTime());
+        assertEquals(eventTimeInStockholm, eventDateTimeHelsinki.toLocalTime().minusHours(1));
+        assertEquals(eventTimeInStockholm, eventDateTimeUTC.toLocalTime().plusHours(2));
     }
 }
