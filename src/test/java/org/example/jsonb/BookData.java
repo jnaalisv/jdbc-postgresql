76f1023cc0a7834473d4d6a710896f75d7bb9005
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2020-03-29 14:40:15 +0300

    convert dto to record

--- src/test/java/org/example/jsonb/BookData.java
+++ src/test/java/org/example/jsonb/BookData.java
@@ -1,31 +1,11 @@
 package org.example.jsonb;
 
-import java.beans.ConstructorProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
-public class BookData {
-
-    private final String title;
-    private final List<String> genres;
-    private final Boolean published;
-
-    @ConstructorProperties({"title", "genres", "published"})
-    public BookData(String title, List<String> genres, Boolean published) {
-        this.title = title;
-        this.genres = genres;
-        this.published = published;
-    }
-
-    public String title() {
-        return title;
-    }
-
-    public List<String> genres() {
-        return genres;
-    }
-
-    public Boolean published() {
-        return published;
-    }
-
+public record BookData(
+        @JsonProperty("title") String title
+        , @JsonProperty("genres") List<String> genres
+        , @JsonProperty("published") Boolean published) {
 }
